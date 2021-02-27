4b88fc7409ac2431ca6fe216b92b819b7095c2b8
mindfulme <go.go.gg.rostislav@gmail.com>
2021-02-27 20:43:37 +0200

    init

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0x52008784Dc2f6d7b591A490fEB8394625b8B0A5b',
+    97: '',
+  },
+  masterChef: {
+    56: '0x154051CaEB6F623D78794885C6977E2BfEC908b3',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
